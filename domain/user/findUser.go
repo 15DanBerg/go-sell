26f4488ceb3a0e161edda5393b94c0fe1ffc99c1
15DanBerg <daniel.berg031@gmail.com>
2024-06-20 14:54:15 -0300

    chore: adding user routes

---
+++ domain/user/findUser.go
@@ -0,0 +1,11 @@
+package user
+
+import "github.com/gin-gonic/gin"
+
+func FindAllUsers(c *gin.Context) {
+
+}
+
+func FindUserByID(c *gin.Context) {
+
+}
--- routes/routes.go
+++ routes/routes.go
@@ -2,16 +2,22 @@ package routes
 
 import (
 	"github.com/15DanBerg/go-sell/domain/product"
+	"github.com/15DanBerg/go-sell/domain/user"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.RouterGroup) {
 	//Product routes
 	r.GET("/product/findById/:productId", product.FindProductByID)
-	r.GET("/product/findAll", product.FindAllProduct)
+	r.GET("/product/findAll", product.FindAllProducts)
 	r.POST("/product/create", product.CreateProduct)
 	r.PUT("/product/update/:productId", product.UpdateProduct)
 	r.DELETE("/product/delete", product.DeleteProdoct)
 
 	//Users Routes
+	r.GET("/user/findById/:userId", user.FindUserByID)
+	r.GET("/user/findAll", user.FindAllUsers)
+	r.POST("/user/create", user.AddingUser)
+	r.PUT("/user/update/:userId", user.UpdateUser)
+	r.DELETE("/user/delete", user.DeleteUser)
 }
