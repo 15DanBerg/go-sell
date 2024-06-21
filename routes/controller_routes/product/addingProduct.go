0bb6c1e5c320100e97289985708b031322dcb8b3
15DanBerg <daniel.berg031@gmail.com>
2024-06-21 13:53:24 -0300

    chore: usin objects in post request

--- routes/controller_routes/product/addingProduct.go
+++ routes/controller_routes/product/addingProduct.go
@@ -1,11 +1,22 @@
 package product
 
 import (
+	"fmt"
+
 	rest_err "github.com/15DanBerg/go-sell/config/err"
+	request "github.com/15DanBerg/go-sell/model/request/product"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateProduct(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Request error")
-	c.JSON(err.Code, err)
+	var productRequest request.ProductRequest
+
+	err := c.ShouldBindJSON(&productRequest)
+	if err != nil {
+		rest_err := rest_err.NewBadRequestError(fmt.Sprintf("There are some icorrect fields, error=%s\n", err.Error()))
+
+		c.JSON(rest_err.Code, rest_err)
+		return
+	}
+	fmt.Println(productRequest)
 }
--- routes/controller_routes/user/addingUser.go
+++ routes/controller_routes/user/addingUser.go
@@ -1,7 +1,23 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	rest_err "github.com/15DanBerg/go-sell/config/err"
+	request "github.com/15DanBerg/go-sell/model/request/user"
+	"github.com/gin-gonic/gin"
+)
 
 func AddingUser(c *gin.Context) {
+	var userRequest request.UserRequest
 
+	err := c.ShouldBindJSON(&userRequest)
+	if err != nil {
+		rest_err := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+
+		c.JSON(rest_err.Code, rest_err)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
