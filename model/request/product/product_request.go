3d8d6f3f0b1d1566b519c04b07d5b403f1c1170c
15DanBerg <daniel.berg031@gmail.com>
2024-06-24 11:42:27 -0300

    chore: adding max value

--- model/request/product/product_request.go
+++ model/request/product/product_request.go
@@ -4,5 +4,5 @@ type ProductRequest struct {
 	Title       string  `json:"title" binding:"required,max=255"`
 	Amount      int     `json:"amount" binding:"required"`
 	Price       float64 `json:"price" binding:"required"`
-	Description string  `json:"description" binding:""`
+	Description string  `json:"description" binding:"max=255"`
 }
